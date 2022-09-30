8b1285b5cd20feaac1aac34ca1ef94a912c3665d
Omar Shishani <omarshishanischool@gmail.com>
2022-09-30 03:12:43 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react'; 
 
 function App() {
+
+  const [time, setTime] = useState(0);
+  const [title, setTitle] = useState('blank');
+
+  useEffect(() => {
+    setTitle('Not Blank'); 
+  });
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {title}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
