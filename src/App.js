fafcec36e4b23c38bc4b36e3a73837a633b0e384
Omar Shishani <omarshishanischool@gmail.com>
2022-10-02 17:59:23 -0400

    Add current time

--- src/App.js
+++ src/App.js
@@ -5,18 +5,30 @@ import React, { useEffect, useState } from 'react';
 function App() {
 
   const [time, setTime] = useState(0);
+  const [currentTime, setCurrentTime] = useState(Date().toLocaleString());
   const [title, setTitle] = useState('blank');
-
+  const [seconds, setSeconds] = useState(0);
+  
+  console.log("yes")
   useEffect(() => {
-    setTitle('Not Blank'); 
-  });
+    const interval = setInterval(() => {
+      setSeconds(seconds => seconds + 1);
+      setCurrentTime(Date().toLocaleString());
+
+      console.log('this will run every second');
+    }, 1000); 
+    return () => clearInterval(interval); 
+  }, []);
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          {title}
+          {currentTime}
+        </p>
+        <p>
+          {seconds}
         </p>
       </header>
     </div>
