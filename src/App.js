d3cddf41ca2f7ac9349921d80e7bb057c74279a4
Omar Shishani <omarshishanischool@gmail.com>
2022-10-02 18:55:47 -0400

    Add function to check that alarm goes off

--- src/App.js
+++ src/App.js
@@ -6,12 +6,20 @@ function App() {
 
   const alarmGetMinutes = () => (new Date()).getMinutes();
   const alarmGetSeconds = () => (new Date()).getSeconds();
+  
+  const getMinutesNine = () => {
+    const minutes = (new Date()).getMinutes();
+    const minutesNine = Math.floor(minutes/10) * 10 + 9;
+    return minutesNine;
+  }
+
 
   const [time, setTime] = useState(0);
   
   const [currentTime, setCurrentTime] = useState(Date().toLocaleString());
   
   const [currentTimeMinutes, setCurrentTimeMinutes] = useState( alarmGetMinutes() );
+  const [currentTimeMinutesNine, setCurrentTimeMinutesNine] = useState( alarmGetMinutes() );
 
   const [currentTimeHours, setCurrentTimeHours] = useState((new Date()).getHours());
   const [currentTimeSeconds, setCurrentTimeSeconds] = useState(alarmGetSeconds());
@@ -26,6 +34,16 @@ function App() {
       setCurrentTime(Date().toLocaleString());
       setCurrentTimeMinutes( alarmGetMinutes() );
       setCurrentTimeSeconds( alarmGetSeconds() );
+
+      // Check if alarm is on that time 
+      if (
+        currentTimeHours === 18
+        && 
+        currentTimeMinutes === 55
+        
+        ){
+        console.log('alarm go');
+      }
 
       console.log('this will run every second');
     }, 1000); 
@@ -57,12 +75,18 @@ function App() {
         <p>
           {currentTimeSeconds}
         </p>
+        <p>
+          Minutes Nine: 
+        </p>
+        <p>
+          {getMinutesNine()}
+        </p>
         <button>
           <div>
             Set alarm for: 
           </div>
           <div>
-            6:09pm
+            {currentTimeHours}:{getMinutesNine()}
           </div>
         </button>
       </header>
